bcb08b9c775b17f78f7d58f5fe59b11f11e39260
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2022-11-24 14:14:58 +0100

    feat: implemented struct for commit data

--- src/git/mod.rs
+++ src/git/mod.rs
@@ -1,62 +1,9 @@
-use crate::error::{Error, ErrorKind};
-use crate::git::LoadedRepository::{LocalRepo, WebRepo};
-use git2::Repository;
-use log::{debug, error};
+mod util;
+
+use git2::{Repository, Time};
 use std::fmt::{Display, Formatter};
 use std::path::Path;
 use temp_dir::TempDir;
-
-/// Clones a repository into a temporary directory, or loads an existing repository from the filesystem.
-///
-/// # Errors
-/// Returns an ErrorKind::RepoCloneError, iff the given string literal was interpreted as
-/// repository url and cloning the repository failed.  
-///
-/// Returns an ErrorKind::RepoLoadError, iff the given string literal was interpreted as path
-pub fn clone_or_load(repo_location: &RepoLocation) -> Result<LoadedRepository, Error> {
-    match repo_location {
-        RepoLocation::FileSystem(path) => {
-            debug!("loading repo from {}", repo_location);
-            match Repository::open(path) {
-                Ok(repo) => {
-                    debug!("loaded {} successfully", repo_location);
-                    Ok(LocalRepo {
-                        path: String::from(repo_location.to_str()),
-                        repository: repo,
-                    })
-                }
-                Err(error) => {
-                    error!("was not able to load {}; reason: {}", repo_location, error);
-                    Err(Error::new(ErrorKind::RepoLoadError(error)))
-                }
-            }
-        }
-        RepoLocation::Website(url) => {
-            debug!("started cloning of {}", repo_location);
-            // In case of repositories hosted online
-            // Create a new temporary directory into which the repo can be cloned
-            let temp_dir = TempDir::new().unwrap();
-
-            // Clone the repository
-            let repo = match Repository::clone(url, temp_dir.path()) {
-                Ok(repo) => {
-                    debug!("cloned {} successfully", repo_location);
-                    repo
-                }
-                Err(error) => {
-                    error!("was not able to clone {}; reason: {}", repo_location, error);
-                    return Err(Error::new(ErrorKind::RepoCloneError(error)));
-                }
-            };
-
-            Ok(WebRepo {
-                url: String::from(*url),
-                repository: repo,
-                directory: temp_dir,
-            })
-        }
-    }
-}
 
 pub enum RepoLocation<'a> {
     FileSystem(&'a Path),
@@ -99,34 +46,31 @@ pub enum LoadedRepository {
     },
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct CommitData {
+    id: String,
+    message: String,
+    diff: Vec<String>,
+    author: String,
+    committer: String,
+    time: Time,
+}
 
-    fn init() {
-        let _ = env_logger::builder().is_test(true).try_init();
-    }
-
-    #[test]
-    fn open_local_repo() {
-        init();
-        use std::env;
-        // We try to open this project's repository
-        let path_buf = env::current_dir().unwrap();
-        let location = RepoLocation::FileSystem(path_buf.as_path());
-        let loaded_repo = clone_or_load(&location).unwrap();
-        if let LocalRepo { path, .. } = loaded_repo {
-            assert_eq!(path, location.to_str());
-        }
-    }
-
-    #[test]
-    fn clone_remote_repo() {
-        init();
-        let location = RepoLocation::Website("https://github.com/rust-lang/git2-rs.git");
-        let loaded_repo = clone_or_load(&location).unwrap();
-        if let WebRepo { url, .. } = loaded_repo {
-            assert_eq!(url, location.to_str());
+impl CommitData {
+    pub fn new(
+        id: String,
+        message: String,
+        diff: Vec<String>,
+        author: String,
+        committer: String,
+        time: Time,
+    ) -> Self {
+        CommitData {
+            id,
+            message,
+            diff,
+            author,
+            committer,
+            time,
         }
     }
 }
