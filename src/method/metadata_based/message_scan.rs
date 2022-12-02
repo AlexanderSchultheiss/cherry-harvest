7a82da31f5f7112d79cba70949e0f7626dea9518
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2022-12-02 16:21:25 +0100

    docs: description of MessageScan

--- src/method/metadata_based/message_scan.rs
+++ src/method/metadata_based/message_scan.rs
@@ -4,7 +4,23 @@ use crate::{CommitPair, SearchResult};
 use std::collections::HashSet;
 
 #[derive(Default)]
-// TODO: documentation
+/// The MessageScan method identifies cherry picks based on the automatically created text in a commit message.
+///
+/// If a cherry pick is done with the *-x* option (i.e., `git cherry-pick -x SOME_HASH`), git will
+/// insert the text `(cherry picked from commit SOME_HASH)` into the commit message.
+///
+/// This method exploits these auto-generated message text for cherry pick recognition. First,
+/// it searches the commit message of each commit for the text *'(cherry picked from commit '*.
+/// If it finds the text in a commit message, it extracts the hash of the cherry-picked commit.
+/// Lastly, it initializes a *SearchResult* for the commit whose message contained the text and the commit
+/// identified by the extracted hash.
+///
+/// Under the assumption that commit messages have not been corrupted with invalid
+/// *(cherry picked from...)* text deliberately, this method will only return correct results.
+/// However, the method cannot guarantee to find all cherry picks, because the commit message text
+/// is only generated if developers specify the *-x* option while using
+/// `git cherry-pick`. Thus, the method cannot find cherry picks that were done without the option,
+/// or that were done manually (i.e., copy-paste).  
 pub struct MessageScan();
 
 const NAME: &str = "MessageScan";
