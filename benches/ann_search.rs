ca1c4ac6970d0b762177d379d4547db7143ed52f
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2023-02-07 20:14:15 +0100

    misc: removed comment

--- benches/ann_search.rs
+++ benches/ann_search.rs
@@ -10,7 +10,6 @@ fn repo_location() -> RepoLocation<'static> {
 
 fn search_call() {
     let search_method = cherry_harvest::ANNMatch::default();
-    // Last search runtime ... never finished ... took too long
     let _ = cherry_harvest::search_with(&repo_location(), search_method);
 }
 
