ed45133e432e2ab8b2b2c589d87acbcbbe7a56c9
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2023-02-21 18:58:41 +0100

    profiling: simple benchmark for repository and commit loading

---
+++ benches/repo_loading.rs
@@ -0,0 +1,20 @@
+use cherry_harvest::{collect_commits, git, RepoLocation};
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::path::PathBuf;
+
+const DATASET: &str = "/home/alex/data/VEVOS_Simulation";
+fn repo_location() -> RepoLocation {
+    RepoLocation::Filesystem(PathBuf::from(DATASET))
+}
+
+pub fn commit_loading(c: &mut Criterion) {
+    c.bench_function("collect_commits", |b| {
+        b.iter(|| {
+            let repository = git::clone_or_load(&repo_location()).unwrap();
+            collect_commits(&[repository]);
+        })
+    });
+}
+
+criterion_group!(benches, commit_loading);
+criterion_main!(benches);
