3678d0fb9d050cbf17e9cc64ba9436654dcf34f6
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2024-03-22 12:23:32 +0100

    feat: implement saving and loading of repo samples

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,8 @@
 use git2::Error as G2Error;
 use octocrab::Error as GHError;
+use serde_yaml::Error as SerdeError;
 use std::fmt::{self, Display, Formatter};
+use std::io::Error as IOError;
 
 #[derive(Debug)]
 pub enum ErrorKind {
@@ -10,14 +12,16 @@ pub enum ErrorKind {
     DiffParse(String),
     ANNPreprocessing(String),
     GitHub(GHError),
+    Serde(SerdeError),
+    IO(IOError),
 }
 
 #[derive(Debug)]
-pub struct Error(Box<ErrorKind>);
+pub struct Error(ErrorKind);
 
 impl Error {
     pub fn new(error_kind: ErrorKind) -> Self {
-        Self(Box::new(error_kind))
+        Self(error_kind)
     }
 }
 
@@ -33,6 +37,12 @@ impl Display for ErrorKind {
             Self::GitHub(error) => {
                 write!(f, "{error}")
             }
+            ErrorKind::Serde(error) => {
+                write!(f, "{error}")
+            }
+            ErrorKind::IO(error) => {
+                write!(f, "{error}")
+            }
         }
     }
 }
@@ -44,3 +54,15 @@ impl Display for Error {
 }
 
 impl std::error::Error for Error {}
+
+impl From<SerdeError> for Error {
+    fn from(error: serde_yaml::Error) -> Self {
+        Self(ErrorKind::Serde(error))
+    }
+}
+
+impl From<IOError> for Error {
+    fn from(error: std::io::Error) -> Self {
+        Self(ErrorKind::IO(error))
+    }
+}
